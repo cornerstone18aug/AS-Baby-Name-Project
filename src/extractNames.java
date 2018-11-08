c433a0cfa606beb6cbcb52da5edd1a517e9807d6
ayns <peenats1203@gmail.com>
2018-11-09 08:29:20 +0900

    added every name and rank

--- src/extractNames.java
+++ src/extractNames.java
@@ -7,61 +7,55 @@ import java.util.regex.Pattern;
 public class extractNames {
 
     public static void main(String[] args) {
-        String readFile = readFile();
+        for (int i=0; i<=9; i++) {
+            String readFile = readFile(i);
 
-        Pattern patternYear = Pattern.compile("Popularity\\sin\\s(\\d\\d\\d\\d)");
-        Pattern patternNameAndRank = Pattern.compile("<td>(\\d+)</td><td>(\\w+)</td>\\<td>(\\w+)</td>");
+            Pattern patternYear = Pattern.compile("Popularity\\sin\\s(\\d\\d\\d\\d)");
+            Pattern patternNameAndRank = Pattern.compile("<td>(\\d+)</td><td>(\\w+)</td>\\<td>(\\w+)</td>");
 
-        Matcher matcherYear = patternYear.matcher(readFile);
-        Matcher matcherNameAndRank = patternNameAndRank.matcher(readFile);
+            Matcher matcherYear = patternYear.matcher(readFile);
+            Matcher matcherNameAndRank = patternNameAndRank.matcher(readFile);
 
-
-        Map<String, String> map_babynames = new HashMap<>();
-        List<String> listYear = new ArrayList<>();
-        while (matcherYear.find()) {
-            listYear.add(matcherYear.group(1));
-        }
-
-        while (matcherNameAndRank.find()) {
-            map_babynames.put(matcherNameAndRank.group(1), matcherNameAndRank.group(2) + matcherNameAndRank.group(3));
-        }
-
-        List<Map.Entry<String, String>> list_babynames = new ArrayList<Map.Entry<String, String>>(map_babynames.entrySet());
-        Collections.sort(list_babynames, new Comparator<Map.Entry<String, String>>() {
-            public int compare(Map.Entry<String, String> obj1, Map.Entry<String, String> obj2) {
-                return obj1.getValue().compareTo(obj2.getValue());
+            Map<String, String> map_babynames = new HashMap<>();
+            String listYear = "";
+            while (matcherYear.find()) {
+                listYear = matcherYear.group(1);
             }
-        });
 
-        for (Map.Entry<String, String> babyname : list_babynames) {
-            System.out.printf("%3s:  %s", babyname.getKey(), babyname.getValue());
-            System.out.println();
+            while (matcherNameAndRank.find()) {
+                map_babynames.put(matcherNameAndRank.group(1), matcherNameAndRank.group(2) + matcherNameAndRank.group(3));
+            }
+
+            List<Map.Entry<String, String>> list_babynames = new ArrayList<Map.Entry<String, String>>(map_babynames.entrySet());
+            Collections.sort(list_babynames, (obj1, obj2) -> obj1.getValue().compareTo(obj2.getValue()));
+
+            System.out.println(listYear);
+            for (Map.Entry<String, String> babyname : list_babynames) {
+                System.out.printf("%3s:  %s", babyname.getKey(), babyname.getValue());
+                System.out.println();
+            }
         }
     }
 
-//        for(Map.Entry<String, String> entry : hashMap.entrySet()) {
-//            String key = entry.getKey();
-//            String value = entry.getValue();
-//            System.out.println(key + value);
-//        }    }
+    private static String readFile(int i) {
 
-    private static String readFile() {
-
-        try {
-            FileReader fileReader = new FileReader("src/babynames/baby1990.html");
-            BufferedReader br = new BufferedReader(fileReader);
-            String strLine = "";
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                strLine += line;
+            try {
+                ArrayList<String> fileList = new ArrayList<>();
+                String year = Integer.toString(1990 + (i * 2));
+                    FileReader fileReader = new FileReader("src/babynames/baby" + year + ".html");
+                    BufferedReader br = new BufferedReader(fileReader);
+                    String strLine = "";
+                    for (String line = br.readLine(); line != null; line = br.readLine()) {
+                        strLine += line;
+                    }
+                    fileReader.close();
+                    br.close();
+                    return strLine;
+            } catch (Exception e) {
+                System.err.println("Error: " + e.getMessage());
+                return null;
             }
-            fileReader.close();
-            br.close();
-            return strLine;
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-            return "";
         }
-    }
 
 }
 
