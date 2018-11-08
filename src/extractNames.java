3699f17e1142a81d90cdf0373a6ef8ab85d63bf8
ayns <peenats1203@gmail.com>
2018-11-08 12:22:02 +0900

    added HashMap

--- src/extractNames.java
+++ src/extractNames.java
@@ -1,17 +1,50 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class extractNames {
 
     public static void main(String[] args) {
-        String s = readFile();
-        System.out.println(s);
-    }
+        String readFile = readFile();
+
+        Pattern patternYear = Pattern.compile("Popularity\\sin\\s(\\d\\d\\d\\d)");
+        Pattern patternNameAndRank = Pattern.compile("<td>(\\d+)</td><td>(\\w+)</td>\\<td>(\\w+)</td>");
+
+        Matcher matcherYear = patternYear.matcher(readFile);
+        Matcher matcherNameAndRank = patternNameAndRank.matcher(readFile);
+
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        List<String> listYear = new ArrayList<>();
+        List<String> listRank = new ArrayList<>();
+        List<String> listName = new ArrayList<>();
+        while (matcherYear.find()) {
+            listYear.add(matcherYear.group(1));
+        }
+
+        while (matcherNameAndRank.find()) {
+//            listRank.add(matcherNameAndRank.group(1));
+//            listName.add(matcherNameAndRank.group(2) + matcherNameAndRank.group(3));
+            hashMap.put(matcherNameAndRank.group(1), matcherNameAndRank.group(2) + matcherNameAndRank.group(3));
+        }
+
+        for(Map.Entry<String, String> entry : hashMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + value);
+            // do what you have to do here
+            // In your case, another loop.
+        }    }
 
     private static String readFile() {
 
         try {
-            FileReader fileReader = new FileReader("src/Baby/babynames/baby1990.html");
+            FileReader fileReader = new FileReader("src/babynames/baby1990.html");
 //            FileInputStream fstream = new FileInputStream("src/Baby/babynames/baby1990.html");
             BufferedReader br = new BufferedReader(fileReader);
             String strLine = "";
